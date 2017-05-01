a620ccca4d0c32c889a9326a14132502b0a2937e
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-05-01 12:09:36 -0700

    add newsletter html page that shows different information if admin is logged in or not.

--- src/SonOfCod/Controllers/NewsletterController.cs
+++ src/SonOfCod/Controllers/NewsletterController.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+using SonOfCod.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace SonOfCod.Controllers
 {
     public class NewsletterController : Controller
     {
-        // GET: /<controller>/
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public NewsletterController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<SelectListItem> reasons = new List<SelectListItem>();
+            reasons.Add(new SelectListItem() { Text = "Buy in bulk", Value = "Buy in bulk" });
+            reasons.Add(new SelectListItem() { Text = "Household shopping", Value = "Household shopping" });
+            reasons.Add(new SelectListItem() { Text = "Stock restaurant", Value = "Stock restaurant" });
+            reasons.Add(new SelectListItem() { Text = "Looking for deals", Value = "Looking for deals" });
+            reasons.Add(new SelectListItem() { Text = "Wider variety of seafood", Value = "Wider variety of seafood" });
+            reasons.Add(new SelectListItem() { Text = "Online shopping convenience", Value = "Online shopping convenience" });
+            ViewBag.Reasons = reasons;
+            ViewBag.Visitors = _db.Visitors.ToList();
             return View();
         }
     }
