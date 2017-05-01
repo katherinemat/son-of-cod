5e23d3b654a8247e3d87192eda4234352741b716
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-05-01 10:32:47 -0700

    add initial project setup with admin controller and db and appuser models.

---
+++ src/SonOfCod/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SonOfCod.Models;
+
+namespace SonOfCod.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AdminController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
